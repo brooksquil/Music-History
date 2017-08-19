253a4c9bc754db8904e3d25afe70abc619d5ba05
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-08-19 01:45:20 -0500

    finished all but printing to the DOM, will get to that shortly

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-"use strict";
-
 console.log("Main.js");
+
 
 
 var songs = [];
@@ -10,3 +9,40 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+// console.log(songs);
+
+
+// Add one song to the beginning (unshift()):
+let newSong = "The Story - by Brandi Carlile on the album The Story";
+let addSongFront = songs.unshift(newSong);
+console.log("Song Added to Beggining of Array:", songs);
+
+//and the end of the array (push()): 
+let newSong2 = "Jackie's Strength by Tori Amos on the album Choir Girl Hotel ";
+let addSongEnd = songs.push(newSong2);
+console.log("Song Added to End of Array", songs);
+
+
+
+//Find and replace the > character in each item with a - character.
+
+for (let i = 0; i <= songs.length; i++) {
+    songs[i] = songs[i].replace(/>/g, "-");
+    console.log("Removed '>' replace with '-':", songs);
+    //Loop over the array, and remove any words or characters that obviously don't belong:
+    songs[i] = songs[i].replace(/[*/]/g, "");
+    songs[i] = songs[i].replace(/[(/]/g, "");
+    songs[i] = songs[i].replace(/[!/]/g, "");
+    console.log("Removed Special Characters:", songs);
+
+    var newArray = songs;
+    console.log("New Array:", newArray);
+
+
+};
+$('#container-print').append(newArray);
+
+
+//Add each string to the DOM in index.html in the main content area. Example output:
+// {Song name} by {Artist} on the album {Album}

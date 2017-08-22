96f8e9fab7830688083cb7615a16cbfcfd86036a
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-08-21 20:50:49 -0500

    first commit on new version-3 branch, added js files, changes to HTML

--- javascripts/list.js
+++ javascripts/list.js
@@ -1,7 +1,16 @@
 "use strict";
 console.log("Main.js");
 
+// var listLink = document.getElementById("link-list");
+// var listView = document.getElementById("list-view");
 
+// listLink.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   addView.classList.add("hidden");
+
+//   listView.classList.add("visible");
+//   listView.classList.remove("hidden");
+// });
 
 var songs = [];
 
